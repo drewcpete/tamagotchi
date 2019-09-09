e6ff9b06f9338307ee38c2e339464bc3c97a1eb4
Andrew Peterson and sofia giordano <sofia@>
2019-09-09 11:03:24 -0700

    pass three tests for decrementing points with a timer

--- spec/tamagotchi-spec.js
+++ spec/tamagotchi-spec.js
@@ -17,3 +17,35 @@ describe("Tama", function() {
     expect(tammy.hunger).toBe(95)
   });
 });
+
+describe("Tama", function(){
+  let tammy;
+  beforeEach(function() {
+    jasmine.clock().install();
+  });
+  afterEach(function(){
+    jasmine.clock().uninstall();
+  });
+
+  it("should test to see if the timer is decrementing the happiness points", function(){
+    tammy = new Tama();
+    jasmine.clock().tick(30001)
+    expect(tammy.happiness).toBe(95)
+  });
+});
+
+describe("Tama", function(){
+  let tammy;
+  beforeEach(function() {
+    jasmine.clock().install();
+  });
+  afterEach(function(){
+    jasmine.clock().uninstall();
+  });
+
+  it("should test to see if the timer is decrementing the exhaustion points", function(){
+    tammy = new Tama();
+    jasmine.clock().tick(30001)
+    expect(tammy.exhaustion).toBe(95)
+  });
+});
